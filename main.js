e3b499cac8fb9b2909fe8615bd3d12c969093d9b
Valdis Saulespurens <valdis.saulespurens@gmail.com>
2018-11-13 10:57:21 +0200

    Fixed XSS and EventListeners

--- main.js
+++ main.js
@@ -9,18 +9,41 @@ function addNewItem () {
     let myTodoList = document.getElementById("main-todo-list");
 
     let newItem = document.createElement('li');
+    let newDiv = document.createElement('div');
+    let newCheckbox = document.createElement('input');
+    newCheckbox.setAttribute('type', 'checkbox');
+    let newSpan = document.createElement('span');
+    let newText = document.createTextNode(textInput);
+    let newButton = document.createElement('input');
+    newButton.setAttribute('type', 'button');
 
-    newItem.innerHTML=`
-            <div>
-                <input type="checkbox">
-                <span>${textInput}</span>
-                <input type="button">
-            </div>
-    `;
-
+    newSpan.appendChild(newText);
+    newDiv.appendChild(newCheckbox);
+    newDiv.appendChild(newSpan);
+    newDiv.appendChild(newButton);
+    newItem.appendChild(newDiv);
     myTodoList.appendChild(newItem);
+
+    // newItem.innerHTML=`
+    //         <div>
+    //             <input type="checkbox">
+    //             <span>${textInput}</span>
+    //             <input type="button">
+    //         </div>
+    // `;
+
+    
 
     // And we clear the input field
     textInputEl.value =""
 
 }
+
+function main () {
+    console.log("Running Main");
+    // document.getElementById("new-item-button").onclick = addNewItem;
+    document.getElementById("new-item-button").addEventListener('click', addNewItem);
+    // document.getElementById("new-item-button").addEventListener('click', () => {alert("Hello")});
+}
+
+main();

f55792809003a70e69fe9f57f9dc37bdab90f63f
Valdis Saulespurens <valdis.saulespurens@gmail.com>
2018-11-14 12:24:43 +0200

    Add Mark Items functionality

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function addNewItem () {
+function addNewItem() {
     console.log("Adding New Items");
     // Getting Text to be used for new item
     let textInputEl = document.getElementById("new-item-text");
@@ -12,6 +12,8 @@ function addNewItem () {
     let newDiv = document.createElement('div');
     let newCheckbox = document.createElement('input');
     newCheckbox.setAttribute('type', 'checkbox');
+    //TODO add handler here
+    newCheckbox.addEventListener('click', toggleItemsDone);
     let newSpan = document.createElement('span');
     newSpan.classList.add("d-inline-block","text-truncate","list-item");
     let newText = document.createTextNode(textInput);
@@ -54,11 +56,43 @@ function closeEditItemWindow(event) {
     event.target.classList.remove("editable-item");
 }
 
+function handleKeyboardInput(event) {
+    if (event.key === "Enter") { 
+        console.log("Pressed enter");
+        event.preventDefault();
+        addNewItem();
+    }
+    console.log("Handling keyboard event");
+    console.log("Pressed key:"+event.key);
+}
+
+function toggleItemsDone(event) {
+    console.log("Marking event done");
+    let spanArray = event.target.parentNode.getElementsByTagName('span');
+    if (spanArray.length === 0 ) return;
+
+    // if (event.target.checked) {
+    //     // spanArray[0].innerHTML = `<s>${spanArray[0].innerHTML}</s>`;
+    //     spanArray[0].style["text-decoration"] = "line-through";
+    // } else {
+    //     // spanArray[0].innerHTML = spanArray[0].innerHTML.replace("<s>","").replace("</s>","");
+    //     spanArray[0].style["text-decoration"] = "none";
+    // }
+    spanArray[0].style["text-decoration"] = event.target.checked ? "line-through" : "none";
+}
+
+function init() {
+    let myInput = document.getElementById('new-item-text');
+    //ADD code here
+    myInput.addEventListener('keydown', handleKeyboardInput);
+
+    document.getElementById("new-item-button").addEventListener('click', addNewItem);
+}
+
 function main () {
     console.log("Running 'my little lamb' Main");
-    // document.getElementById("new-item-button").onclick = addNewItem;
-    document.getElementById("new-item-button").addEventListener('click', addNewItem);
-    // document.getElementById("new-item-button").addEventListener('click', () => {alert("Hello")});
+    //add Init Code
+    init();
 }
 
 main();
